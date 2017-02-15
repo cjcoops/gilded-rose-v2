14d463696fb2cf97c410449f996e37891b05998d
Chris Cooper <cjcooper252@gmail.com>
2017-02-15 17:58:38 +0000

    moves item logic into case statement

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -19,38 +19,22 @@ class GildedRose
   def item_handler(item)
     case item.name
       when "normal"
-        return normal_update_quality(item)
+        item = Normal.new(item.sell_in, item.quality)
+        item.update_quality
+        item
       when "Aged Brie"
-        return aged_brie_update_quality(item)
+        item = Brie.new(item.sell_in, item.quality)
+        item.update_quality
+        item
       when "Sulfuras, Hand of Ragnaros"
-        return sulfuras_update_quality(item)
+        item = Sulfuras.new(item.sell_in, item.quality)
+        item.update_quality
+        item
       when "Backstage passes to a TAFKAL80ETC concert"
-        return backstage_passes_update_quality(item)
+        item = BackstagePass.new(item.sell_in, item.quality)
+        item.update_quality
+        item
     end
-  end
-
-  def normal_update_quality(item)
-    new_item = Normal.new(item.sell_in, item.quality)
-    new_item.update_quality
-    new_item
-  end
-
-  def aged_brie_update_quality(item)
-    new_item = Brie.new(item.sell_in, item.quality)
-    new_item.update_quality
-    new_item
-  end
-
-  def sulfuras_update_quality(item)
-    new_item = Sulfuras.new(item.sell_in, item.quality)
-    new_item.update_quality
-    new_item
-  end
-
-  def backstage_passes_update_quality(item)
-    new_item = BackstagePass.new(item.sell_in, item.quality)
-    new_item.update_quality
-    new_item
   end
 end
 
