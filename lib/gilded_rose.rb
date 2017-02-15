bf3fea98d9f92f0ee761ff2f0d6cdefda500d5ac
Chris Cooper <cjcooper252@gmail.com>
2017-02-15 16:48:52 +0000

    removes legacy if statement

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -6,7 +6,6 @@ class GildedRose
 
   def update_quality()
     @items.each do |item|
-
       case item.name
       when "normal"
         return normal_update_quality(item)
@@ -16,51 +15,6 @@ class GildedRose
         return sulfuras_update_quality(item)
       when "Backstage passes to a TAFKAL80ETC concert"
         return backstage_passes_update_quality(item)
-      end
-
-
-      if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
-        if item.quality > 0
-          if item.name != "Sulfuras, Hand of Ragnaros"
-            item.quality = item.quality - 1
-          end
-        end
-      else
-        if item.quality < 50
-          item.quality = item.quality + 1
-          if item.name == "Backstage passes to a TAFKAL80ETC concert"
-            if item.sell_in < 11
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-            if item.sell_in < 6
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-          end
-        end
-      end
-      if item.name != "Sulfuras, Hand of Ragnaros"
-        item.sell_in = item.sell_in - 1
-      end
-      if item.sell_in < 0
-        if item.name != "Aged Brie"
-          if item.name != "Backstage passes to a TAFKAL80ETC concert"
-            if item.quality > 0
-              if item.name != "Sulfuras, Hand of Ragnaros"
-                item.quality = item.quality - 1
-              end
-            end
-          else
-            item.quality = item.quality - item.quality
-          end
-        else
-          if item.quality < 50
-            item.quality = item.quality + 1
-          end
-        end
       end
     end
   end
@@ -91,8 +45,6 @@ class GildedRose
     item.quality += 1 if item.sell_in < 5
   end
 end
-
-
 
 class Item
   attr_accessor :name, :sell_in, :quality
